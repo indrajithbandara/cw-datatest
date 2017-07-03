a90e192b033e72e03a24721d77efaa6fb2ba74c2
SoftwareLuke <root@softwareluke-Lenovo.(none)>
2017-07-03 19:11:44 +0800

    submit

---
+++ node-selenium/testbaidu.js
@@ -0,0 +1,15 @@
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+
+var driver = new webdriver.Builder()
+    .forBrowser('chrome')
+    .build();
+
+driver.get('https://www.baidu.com');
+driver.findElement(By.id('kw')).sendKeys('webdriver');
+driver.findElement(By.id('su')).click();
+driver.wait(until.titleIs('webdriver_百度搜索'), 1000);
+driver.quit();
+//http://www.cnblogs.com/fnng/p/5854875.html
+//
